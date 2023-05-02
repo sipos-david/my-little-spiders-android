688f772eedf36493dd6978bedf6d1a4c1d6ef071
Sipos Áron Dávid <sipisipos@gmail.com>
2023-05-02 16:50:00 +0200

    Fix failing test

--- backend/src/test/kotlin/dev/shipi/ApplicationTest.kt
+++ backend/src/test/kotlin/dev/shipi/ApplicationTest.kt
@@ -1,21 +1,25 @@
 package dev.shipi
 
+import dev.shipi.data.RoommateStorage
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.server.testing.*
 import kotlin.test.*
 import io.ktor.http.*
 import dev.shipi.plugins.*
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
 
 class ApplicationTest {
     @Test
-    fun testRoot() = testApplication {
+    fun test_GET_Roommate() = testApplication {
         application {
+            configureSerialization()
             configureRouting()
         }
-        client.get("/").apply {
+        client.get("/roommate").apply {
             assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Hello World!", bodyAsText())
+            assertEquals(Json.encodeToString(RoommateStorage.getAllRoommates()), bodyAsText())
         }
     }
 }
