e19c0de8b1bd9c32a1204f1dfd0fea8d48c2ea27
Sipos Áron Dávid <sipisipos@gmail.com>
2023-05-04 19:48:12 +0200

    Add ListScreen design

--- app/src/main/java/dev/shipi/mylittlespiders/presentation/list/ListScreen.kt
+++ app/src/main/java/dev/shipi/mylittlespiders/presentation/list/ListScreen.kt
@@ -1,21 +1,31 @@
 package dev.shipi.mylittlespiders.presentation.list
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Refresh
 import androidx.compose.material.icons.rounded.Add
-import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.LargeTopAppBar
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.tooling.preview.PreviewParameter
 import androidx.compose.ui.tooling.preview.PreviewParameterProvider
+import androidx.compose.ui.unit.dp
 import dev.shipi.mylittlespiders.domain.model.Friend
 import dev.shipi.mylittlespiders.lib.presentation.ViewState
 import dev.shipi.mylittlespiders.presentation.list.view.FriendRow
@@ -39,6 +49,7 @@ fun ListScreen(
     )
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ListView(
     state: ViewState<List<Friend>>,
@@ -51,27 +62,50 @@ fun ListView(
     when (state) {
         is ViewState.Loading -> CircularProgressIndicator()
         is ViewState.Error -> Text(text = "${state.e.message}")
-        is ViewState.Data -> Column {
-            if (!state.isNetworkAvailable) {
-                Text(text = "Network is unavailable!")
-            }
-            Button(onClick = onListRefresh) {
-                Text(text = "Refresh from network")
-            }
-            FloatingActionButton(onClick = onNavigateToAddFriend) {
-                Icon(
-                    imageVector = Icons.Rounded.Add,
-                    contentDescription = "Add new friend!",
-                )
-            }
-            LazyColumn {
-                items(state.data) { friend ->
-                    FriendRow(
-                        friend,
-                        onDeleteFriend,
-                        onNavigateToViewFriend,
-                        onNavigateToUpdateFriend
+        is ViewState.Data -> {
+            Box(
+                modifier = Modifier.fillMaxSize(),
+                contentAlignment = Alignment.BottomEnd
+            ) {
+                FloatingActionButton(
+                    modifier = Modifier.padding(bottom = 16.dp, end = 16.dp),
+                    onClick = onNavigateToAddFriend
+                ) {
+                    Icon(
+                        imageVector = Icons.Rounded.Add,
+                        contentDescription = "Add new friend!",
                     )
+                }
+            }
+            Column {
+                LargeTopAppBar(
+                    title = {
+                        Text(
+                            text = "My Little Spiders",
+                            style = MaterialTheme.typography.headlineMedium
+                        )
+                    },
+                    actions = {
+                        IconButton(onClick = onListRefresh) {
+                            Icon(
+                                imageVector = Icons.Outlined.Refresh,
+                                contentDescription = "Refresh list from network."
+                            )
+                        }
+                    }
+                )
+                if (!state.isNetworkAvailable) {
+                    Text(text = "Network is unavailable!")
+                }
+                LazyColumn(modifier = Modifier.padding(start = 16.dp, end = 16.dp, top = 8.dp)) {
+                    items(state.data) { friend ->
+                        FriendRow(
+                            friend,
+                            onDeleteFriend,
+                            onNavigateToViewFriend,
+                            onNavigateToUpdateFriend
+                        )
+                    }
                 }
             }
         }
